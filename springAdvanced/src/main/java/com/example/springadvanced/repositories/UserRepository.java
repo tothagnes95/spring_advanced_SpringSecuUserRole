4bc551644cbcc41888ce24376479011a4885971b
tothagnes95 <tothagnes95@gmail.com>
2023-06-30 15:18:55 +0200

    working on spring security

---
+++ springAdvanced/src/main/java/com/example/springadvanced/repositories/UserRepository.java
@@ -0,0 +1,16 @@
+package com.example.springadvanced.repositories;
+
+import com.example.springadvanced.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+}
